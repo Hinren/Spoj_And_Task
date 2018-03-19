66e62dde39dceeccf448338b5145c78213f8ab40
Hinren <wisniewskii.patryk@o2.pl>
2018-03-19 16:01:02 +0100

    Work
    Work

---
+++ SPOJ/WI_TRIGR.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string linia;
+            string[] tab;
+            string[] tab2;
+            int m = 0;
+            while ((linia = Console.ReadLine()) != null)
+            {
+                tab = linia.Split();
+                linia = Console.ReadLine();
+                tab2 = linia.Split();
+                m += int.Parse(tab[1]);
+                for (int i = 0; i < tab2.Length; i++)
+                {
+                    if (m == tab2.Length)
+                    {
+                        m = 0;
+                        Console.Write("{0} ", tab2[m]);
+                    }
+                    else
+                        Console.Write("{0} ", tab2[m]);
+
+                    m++;
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
